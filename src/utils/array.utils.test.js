419d30543a8aac20c4fa6beb3fc043c34f556be1
Anton Kurhuzenkau <akurhuzenkau@cloudpassage.com>
2018-08-21 14:17:20 +0300

    add tests

--- src/utils/array.utils.test.js
+++ src/utils/array.utils.test.js
@@ -1,5 +1,6 @@
 import * as arrayUtils from './array.utils.js';
 import { assert } from 'chai';
+import sinon from 'sinon';
 
 describe('arrayUtils', function () {
 
@@ -22,10 +23,38 @@ describe('arrayUtils', function () {
     it('return [0; length) if second argument is omit', () => {
       const length = 5;
       const array = generateArray(length);
-      assert.deepEqual(array, [0, 1, 2, 3, 4]);
+      assert.deepEqual(array, [ 0, 1, 2, 3, 4 ]);
     });
 
-    // TODO check calls of generateItems
+    it('use itemGenerator for array filling', () => {
+      const x2 = index => index * 2;
+      const length = 3;
+      const array = generateArray(length, x2);
+      assert.deepEqual(array, [ 0, 2, 4 ]);
+    });
+
+    it('call itemGenerator for each array index', () => {
+      const itemGenerator = sinon.spy();
+      const length = 4;
+      generateArray(length, itemGenerator);
+      assert.strictEqual(itemGenerator.callCount, length);
+    });
+
+    it('call itemGenerator with index as first argument', () => {
+      const itemGenerator = sinon.spy();
+      generateArray(3, itemGenerator);
+  
+      assert.strictEqual(itemGenerator.firstCall.args[0], 0);
+      assert.strictEqual(itemGenerator.secondCall.args[0], 1);
+      assert.strictEqual(itemGenerator.thirdCall.args[0], 2);
+    });
+
+    it('call itemGenerator with generated array as second argument', () => {
+      const itemGenerator = sinon.spy(index => index);
+      const array = generateArray(1, itemGenerator);
+  
+      assert.strictEqual(itemGenerator.firstCall.args[1], array);
+    });
 
   });
 
