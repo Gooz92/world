3abd6e8bb5dd9fec3cfd2b4bde7a2ebdb3fcc5bb
Anton <kurguzenkov@gmail.com>
2018-09-09 23:29:55 +0300

    use flex and divs

--- src/create-field.js
+++ src/create-field.js
@@ -1,20 +1,35 @@
-import { createTable } from './utils/dom.utils.js';
+import { createElement } from './utils/dom.utils.js';
 
 const classes = [ 'empty', 'obstacle', 'tree', 'person' ];
 
+const TILE_SIZE = 8;
+
 export default function createField(tiles) {
+  const height = tiles.length * TILE_SIZE;
+  const width = tiles[0].length * TILE_SIZE;
   const cells = [];
 
-  const table = createTable(tiles.length, tiles[0].length, (cell, y, x) => {
-    const object = tiles[y][x].object;
-    const objectType = object ? object.type : 0;
-
-    cell.className = classes[objectType];
-
-    cell.id = `tile-${x}-${y}`;
-
-    (cells[y] || (cells[y] = []))[x] = cell;
+  const table = createElement('div', {
+    className: 'field'
   });
+
+  table.style.width = width + 'px';
+  table.style.height = height + 'px';
+
+  for (let y = 0; y < tiles.length; y++) {
+    const row = [];
+    cells.push(row);
+    for (let x = 0; x < tiles[0].length; x++) {
+      const object = tiles[y][x].object;
+      const objectType = object ? object.type : 0;
+      const cell = createElement('div', {
+        id: `tile-${x}-${y}`,
+        className: classes[objectType]
+      });
+      row.push(cell);
+      table.appendChild(cell);
+    }
+  }
 
   return { table, cells };
 }
--- src/utils/dom.utils.test.js
+++ src/utils/dom.utils.test.js
@@ -11,6 +11,14 @@ describe('domUtils', function () {
       const element = createElement(tagName);
       assert.strictEqual(element.tagName.toLowerCase(), tagName);
     });
+
+    it('create html element with given className', () => {
+      const className = 'title';
+      const caption = createElement('h1', { className });
+
+      assert.strictEqual(caption.className, className);
+    });
+
   });
 
 });
