6a4aa9603a20190ede8f61225f39068b3b396148
Anton Kurguzenkov <kurguzenkov@gmail.com>
2018-10-03 22:01:55 +0300

    improve test coverage

--- src/utils/is.utils.test.js
+++ src/utils/is.utils.test.js
@@ -1,4 +1,5 @@
 import * as isUtils from './is.utils.js';
+import { omit } from './object.utils.js';
 import { upperFirst } from './string.utils.js';
 import { assert } from 'chai';
 
@@ -15,10 +16,22 @@ describe('isUtils', function () {
       const method = isUtils[methodName];
       const value = types[typeName];
 
+      const otherTypes = omit(types, [ typeName ]);
+
       describe(methodName, () => {
+  
         it(`should return true for ${typeName}`, () => {
           assert.isTrue(method(value));
         });
+
+        Object.keys(otherTypes)
+          .forEach(otherTypeName => (
+            it(`should return false for ${otherTypeName}`, () => {
+              const value = otherTypes[otherTypeName];
+              assert.isFalse(method(value));
+            })
+          ));
+
       });
 
     });
