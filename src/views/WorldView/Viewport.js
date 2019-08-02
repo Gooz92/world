b964e919cc320a400a81caab41ff62cdd3aaa396
Anton <kurguzenkov@gmail.com>
2019-08-02 13:50:36 +0300

    handle params

--- src/views/WorldView/Viewport.js
+++ src/views/WorldView/Viewport.js
@@ -17,18 +17,23 @@ import TileRenderer from './TileRenderer.js';
 export default class Viewport {
 
   static DEFAULT_CELL_SIZE = 16;
+  static DEFAULT_SIZE = [ 16, 9 ];
 
   constructor(world, options = {}) {
     this.position = [ 0, 0 ];
-    this.size = options.size;
     this.cellSize = Viewport.DEFAULT_CELL_SIZE;
 
+    this.size = Viewport.DEFAULT_SIZE;
     this.world = world;
 
     this.layers = [];
 
     this.container = createElement('#viewport');
 
+    this.setOptions(options);
+  }
+
+  setOptions(options) {
     if (options.onTileClick) {
       this.container.onclick = this.handleMouseEvent((x, y) => {
         options.onTileClick(x, y);
--- src/views/game.js
+++ src/views/game.js
@@ -1,21 +1,92 @@
-import WorldView from './WorldView';
 import createWorld from '../model/create-world.js';
 import ObjectType from 'model/ObjectType.enum.js';
 
+import WorldView from './WorldView';
+import Viewport from './WorldView/Viewport.js';
+import createButtomPanel from './create-buttom-panel';
+
 import getArrowKeyCode from 'utils/common/get-arrow-key-code.js';
 import { upperFirst } from 'utils/common/string.utils.js';
-
+import { paramsHandler, getViewportSize } from '../app.utils.js';
 import { time } from 'utils/common/dev.utils.js';
 import { debounce } from 'utils/common/fn.utils.js';
-import Viewport from './WorldView/Viewport';
-import createButtomPanel from './create-buttom-panel';
-import { paramsHandler, getViewportSize } from '../app.utils.js';
 
-paramsHandler(({ seed, empty }) => {
-  console.log(seed, empty);
+const wv = new WorldView(null, null);
+
+const viewport = new Viewport(null, {
+  tilesSprite: document.getElementById('tiles-sprite')
 });
 
-const world = createWorld();
+let panel;
+
+const getTileInfo = tile => {
+  if (!tile.object) {
+    return 'empty';
+  }
+
+  return tile.object.type.name;
+};
+
+panel = createButtomPanel(() => {
+  wv.tick();
+  panel.updateSelectionInfo(wv.selection);
+});
+
+const main = document.getElementById('main');
+
+main.appendChild(viewport.container);
+
+main.appendChild(panel.element);
+
+paramsHandler(({ seed, empty }) => {
+  const world = createWorld({ seed, empty });
+
+  viewport.world = world;
+  viewport.setOptions({
+    onTileClick: (x, y) => {
+
+      if (panel.objectType === null) {
+        const selection = wv.select(x, y);
+        panel.updateSelectionInfo(selection);
+        return;
+      }
+
+      if (panel.objectType === 0) {
+        wv.clearTile(x, y);
+        panel.updateSelectionInfo(wv.selection);
+        return;
+      }
+
+      const type = ObjectType.fromId(panel.objectType);
+
+      const object = wv.place(x, y, type);
+
+      if (type === ObjectType.PERSON) {
+        object.setStrategy('cutTrees');
+      }
+    },
+    onTileEnter: (x, y) => {
+      const gx = world.getCycleX(wv.viewport.position[0] + x);
+      const gy = world.getCycleY(wv.viewport.position[1] + y);
+
+      const tile = world.getTile(gx, gy);
+      panel.updateTileInfo(gx + ';' + gy + ' - ' + getTileInfo(tile));
+    }
+  });
+
+  wv.world = world;
+  wv.viewport = viewport;
+
+  if (viewport.layers.length === 0) {
+    wv.initLayers();
+  }
+
+  const [ w, h ] = getViewportSize(wv.viewport.container, wv.viewport.cellSize);
+
+  wv.viewport.setSize(w, h);
+
+  wv.viewport.draw();
+});
 
 window.addEventListener('resize', debounce(event => {
   const [ w, h ] = getViewportSize(wv.viewport.container, wv.viewport.cellSize);
@@ -42,64 +113,3 @@ window.addEventListener('keydown', event => {
   }
 });
 
-let panel;
-
-const getTileInfo = tile => {
-  if (!tile.object) {
-    return 'empty';
-  }
-
-  return tile.object.type.name;
-};
-
-const wv = new WorldView(world, new Viewport(world, {
-  size: [ 40, 30 ],
-  tilesSprite: document.getElementById('tiles-sprite'),
-  onTileClick: (x, y) => {
-
-    if (panel.objectType === null) {
-      const selection = wv.select(x, y);
-      panel.updateSelectionInfo(selection);
-      return;
-    }
-
-    if (panel.objectType === 0) {
-      wv.clearTile(x, y);
-      panel.updateSelectionInfo(wv.selection);
-      return;
-    }
-
-    const type = ObjectType.fromId(panel.objectType);
-
-    const object = wv.place(x, y, type);
-
-    if (type === ObjectType.PERSON) {
-      object.setStrategy('cutTrees');
-    }
-  },
-  onTileEnter: (x, y) => {
-    const gx = world.getCycleX(wv.viewport.position[0] + x);
-    const gy = world.getCycleY(wv.viewport.position[1] + y);
-
-    const tile = world.getTile(gx, gy);
-    panel.updateTileInfo(gx + ';' + gy + ' - ' + getTileInfo(tile));
-  }
-}));
-
-wv.initLayers();
-
-panel = createButtomPanel(() => {
-  wv.tick();
-  panel.updateSelectionInfo(wv.selection);
-});
-
-const main = document.getElementById('main');
-
-main.appendChild(wv.viewport.container);
-main.appendChild(panel.element);
-
-const [ w, h ] = getViewportSize(wv.viewport.container, wv.viewport.cellSize);
-
-wv.viewport.setSize(w, h);
-
-wv.viewport.draw();
